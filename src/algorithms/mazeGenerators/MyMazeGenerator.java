c0447cb2078e1b49507ad25ddeeac2811674a998
Maor <bitondor@post.bgu.ac.il>
2021-03-29 14:58:57 +0300

    Finishing the first mile stone of part A: All the maze generator are complete and working Adding the first test to RunMazeGenerator

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -23,49 +23,87 @@ public class MyMazeGenerator extends AMazeGenerator{
             }
         }
         //position of the starting cell in tha maze
-        int[] curCell = {positions[0].getRowIndex(),positions[0].getColumnIndex()};
+        int[] nextCell, curCell = {positions[0].getRowIndex(),positions[0].getColumnIndex()};
         stack.push(curCell);
-        ArrayList<int[]> neighbors = new ArrayList<>();
-        int[] tempPosition = new int[2];
+        ArrayList<int[]> neighbors;
         Random random = new Random();
         int posNum;
-        //Todo check if works
         while(!stack.empty())
         {
-            if(columns > curCell[1]+2 && map[curCell[0]][curCell[1]+2] == 1)
-            {
-                tempPosition[0] = curCell[0];
-                tempPosition[1] = curCell[1]+2;
-                neighbors.add(tempPosition);
-            }
-            if(curCell[1]-2>=0 && map[curCell[0]][curCell[1]-2] == 1)
-            {
-                tempPosition[0] = curCell[0];
-                tempPosition[1] = curCell[1]-2;
-                neighbors.add(tempPosition);
-            }
-            if(rows > curCell[0]+2 && map[curCell[0]+2][curCell[1]] == 1)
-            {
-                tempPosition[0] = curCell[0]+2;
-                tempPosition[1] = curCell[1];
-                neighbors.add(tempPosition);
-            }
-            if(curCell[0]-2>=0 && map[curCell[0]-2][curCell[1]] == 1)
-            {
-                tempPosition[0] = curCell[0]-2;
-                tempPosition[1] = curCell[1];
-                neighbors.add(tempPosition);
-            }
+            curCell = stack.pop();
+            map[curCell[0]][curCell[1]] = 0;
+            neighbors = getNeighbors(map,rows,columns,curCell);
             if(neighbors.size()!=0) {
+                stack.push(curCell);
                 posNum = random.nextInt(neighbors.size());
-                curCell =neighbors.get(posNum);
+                nextCell = neighbors.get(posNum);
+                //we stay at the same row so we moved to the left or to the right
+                if(nextCell[0] == curCell[0])
+                {
+                    //we moved to the right
+                    if(nextCell[1] > curCell[1]){
+                        map[curCell[0]][curCell[1]+1] = 0;
+                    }
+                    //we moved to the left
+                    else{ // nextCell[1] < curCell[1]
+                        map[curCell[0]][curCell[1]-1] = 0;
+                    }
+                }
+                //we stayed at the same column so we moved up or down
+                else if(nextCell[1] == curCell[1])
+                {
+                    //we moved down
+                    if(nextCell[0] > curCell[0]){
+                        map[curCell[0]+1][curCell[1]] = 0;
+                    }
+                    //we moved up
+                    else { // nextCell[0] < curCell[0]
+                        map[curCell[0]-1][curCell[1]] = 0;
+                    }
+                }
+                curCell = nextCell;
                 stack.push(curCell);
             }
-            else
-                curCell = stack.pop();
         }
-        Maze myMaze = new Maze(map,positions[0],positions[1]);
-        return myMaze;
+        // if we want to make the maze harder we can create more dead ends with random cells turning them to 0
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < columns; j++) {
+//                if(map[i][j] == 1) {
+//                    map[i][j] = random.nextInt(2);
+//                }
+//            }
+//        }
+        return new Maze(map,positions[0],positions[1]);
+    }
+
+    private ArrayList<int[]> getNeighbors(int[][] map, int rows, int columns, int[] curCell){
+        ArrayList<int[]> neighbors = new ArrayList<>();
+        int[] tempPosition = new int[2];
+        if(columns > curCell[1]+2 && map[curCell[0]][curCell[1]+2] == 1)
+        {
+            tempPosition[0] = curCell[0];
+            tempPosition[1] = curCell[1]+2;
+            neighbors.add(tempPosition.clone());
+        }
+        if(curCell[1]-2>=0 && map[curCell[0]][curCell[1]-2] == 1)
+        {
+            tempPosition[0] = curCell[0];
+            tempPosition[1] = curCell[1]-2;
+            neighbors.add(tempPosition.clone());
+        }
+        if(rows > curCell[0]+2 && map[curCell[0]+2][curCell[1]] == 1)
+        {
+            tempPosition[0] = curCell[0]+2;
+            tempPosition[1] = curCell[1];
+            neighbors.add(tempPosition.clone());
+        }
+        if(curCell[0]-2>=0 && map[curCell[0]-2][curCell[1]] == 1)
+        {
+            tempPosition[0] = curCell[0]-2;
+            tempPosition[1] = curCell[1];
+            neighbors.add(tempPosition.clone());
+        }
+        return neighbors;
     }
 
 }
